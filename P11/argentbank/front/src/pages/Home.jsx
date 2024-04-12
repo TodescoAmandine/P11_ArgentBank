9366f822b810462087d7966fea4e4c1710f41b51
TodescoAmandine <todescoamandine@yahoo.fr>
2024-04-12 09:10:48 +0200

    first commit

---
+++ P11/argentbank/front/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navigation from "../components/Navigations";
+import Hero from "../components/Hero.jsx";
+import Features from "../components/Features.jsx";
+import Footer from "../components/Footer.jsx";
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation />
+        <main>
+            <Hero/>  
+            <Features/>  
+        </main>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
